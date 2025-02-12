4c36c31e7e697a9fc1ff2256f4cee69038892928
Kris Springer <Acidfrost59@gmail.com>
2025-02-11 17:03:35 -0800

    Initial files for master
    Original files for rewrite. At this time there are many interaction issues to work out.

---
+++ CritterEmote_EmoteMap.lua
@@ -0,0 +1,14 @@
+CritterEmote_EmoteMap = {
+    ["wave"] = "WAVE",
+    ["cheer"] = "CHEER",
+    ["raise"] = "ANGRY",
+    ["kiss"] = "KISS",
+    ["dance"] = "DANCE",
+    ["laugh"] = "LAUGH",
+    ["hug"] = "HUG",
+    ["kneel"] = "KNEEL",
+    ["nod"] = "NOD",
+    ["salute"] = "SALUTE",
+    ["smile"] = "SMILE",
+    ["thank"] = "THANK",
+}
